e999e57df7213afa290a48e3bba77d9f0b28ad1e
RSR <rsrxrsr@gmail.com>
2019-09-27 17:00:58 -0500

    Demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
   production: true
 };
+
+export const CONEXION_BD = {
+  firebase: {
+    apiKey: "AIzaSyDH6oYKzAK5SwMckJTPAVQF-JOuQ7vkwUQ",
+    authDomain: "proionic-007.firebaseapp.com",
+    databaseURL: "https://proionic-007.firebaseio.com",
+    projectId: "proionic-007",
+    storageBucket: "proionic-007.appspot.com",
+    messagingSenderId: "836343510085",
+    appId: "1:836343510085:web:61f97621eb7846dbe4df2c",
+    measurementId: "G-231RSH26DQ"
+  }      
+};
+
